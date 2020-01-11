9e042a57643e319bfe4a8778e3baf55da07a7e41
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2020-01-10 23:48:06 -0500

    Create sendText.py

---
+++ Python/sendText.py
@@ -0,0 +1,92 @@
+import os
+from twilio.rest import Client
+
+''' 
+* Author https://github.com/key6oardWarrior
+* Code Propety of Full Blast S.T.E.A.M
+* Twi
+'''
+
+authFile = open("C:/Users/Lewjb/Documents/Code/FullBlast/FBSTC/Text_TC/AUTH.txt", "r").read()
+sid = authFile[:34]
+authToken = authFile[36:]
+client = Client(sid, authToken)
+
+def howManyNumbers(): # stores all emergancy numbers
+	num = 0
+
+	try:
+		num = int(input("Enter how many numbers you want to text: "))
+	except:
+		print("Enter only numbers")
+		howManyNumbers()
+	
+	if num <= 0:
+		print("Number must be greater than 0")
+		howManyNumbers()
+
+	for i in range(0, num):
+		addNums2File()
+
+def addNums2File():
+	numberFile = open("C:/Users/Lewjb/Documents/Code/FullBlast/FBSTC/Text_TC/textToNumbers.txt", "a")
+	numbers = 0
+
+	try:
+		numbers = int(input("Enter number: "))
+	except:
+		print("Please only enter numbers")
+		addNums2File()
+
+	if len(str(numbers)) != 10:
+		print("Your number must be 10 digits")
+		addNums2File()
+	else:
+		numberFile.write(str(numbers) + " ")
+	numberFile.close()
+
+def sendMessage():
+	numberFile = open("C:/Users/Lewjb/Documents/Code/FullBlast/FBSTC/Text_TC/textToNumbers.txt", "r").read()
+	cnt = 0
+
+	for i in range(0, len(numberFile)): # count how many phone numbers
+		if numberFile[i] == " ":
+			cnt += 1
+
+	lstNums = [""]*cnt
+	j = 0
+
+	for i in range(0, len(numberFile)): # adds numbers to list
+		if numberFile[i] != " ":
+			lstNums[j] += numberFile[i]
+		else:
+			j += 1
+
+	for i in lstNums:
+		message = client.messages.create(
+			body = 'Test',
+			from_ = '+12512205235', 
+			to = i
+		)
+		print(message.sid + ": Sent the message!")
+
+def main():
+	if os.path.exists("C:/Users/Lewjb/Documents/Code/FullBlast/FBSTC/Text_TC/textToNumbers.txt"):
+		isAddNums = input("Do you want to add numbers to the list of numbers? Y/n ")
+		if(isAddNums.upper() == "Y"):
+			howManyNumbers()
+			isSendMessage = input("Do you want to text all numbers? Y/n ")
+			if isSendMessage.upper() == "Y":
+				sendMessage()
+			else:
+				print("Message not sent :(")
+		else:
+			sendMessage()
+	else:
+		numberFile = open("C:/Users/Lewjb/Documents/Code/FullBlast/FBSTC/Text_TC/textToNumbers.txt", "w")
+		numberFile.close()
+		howManyNumbers()
+		main()
+
+if __name__ == "__main__":
+	main()
